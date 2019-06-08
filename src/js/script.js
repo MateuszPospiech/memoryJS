b8ae5190419f58a4d62b82b09fd1f52df2889ebd
MateuszPospiech <pospiechmateusz@gmail.com>
2019-06-08 20:55:23 +0200

    Add some css and array of cards collor

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,5 @@
-console.log('script');
+const cardsColor = ["red", "red", "green", "green", "blue", "blue", "brown", "brown", "yellow", "yellow", "gray", "gray", "cadetblue", "cadetblue", "coral", "coral", "cyan", "cyan"];
+
+let cards = document.querySelectorAll(".card");
+cards = [...cards]; //transform to Array
+
