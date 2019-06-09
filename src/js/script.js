fbb519ef9ff66534ffd2a7e41447f5427fd416cb
MateuszPospiech <pospiechmateusz@gmail.com>
2019-06-09 15:15:48 +0200

    add init function

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,23 @@ const cardsColor = ["red", "red", "green", "green", "blue", "blue", "brown", "br
 let cards = document.querySelectorAll(".card");
 cards = [...cards]; //transform to Array
 
+const clickCard = function(){
+    console.log("click");
+}
+
+const init = function() {
+    cards.forEach(function(card){
+        const position = Math.floor(Math.random()*cardsColor.length);
+        card.classList.add(cardsColor[position]);
+        cardsColor.splice(position, 1) //delete element from Array
+    })
+
+    setTimeout(function(){
+        cards.forEach(function(card){
+            card.classList.add("hidden");
+            card.addEventListener("click", clickCard)
+        })
+    }, 2000)
+}
+
+init()
